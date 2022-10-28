2c81642cb3083370a179d14a9aee172ca1ba3cf2
Daniel Weiß <daniel.weiss@grollmus.de>
2022-10-28 18:33:04 +0200

    move logic to method

--- client/ups.py
+++ client/ups.py
@@ -178,16 +178,20 @@ class INA219:
             value -= 65535
         return value * self._current_lsb
     
+    def getPercent(self,bus_voltage):
+        P = (bus_voltage -3)/1.2*100
+        if(P<0):P=0
+        elif(P>100):P=100
+        return P
+    
 if __name__=='__main__':
  
     # Create an ADS1115 ADC (16-bit) instance.
     ina219 = INA219(addr=0x43)
     while True:
         bus_voltage = ina219.getBusVoltage_V()             # voltage on V- (load side)
-        current = ina219.getCurrent_mA()                   # current in mA
-        P = (bus_voltage -3)/1.2*100
-        if(P<0):P=0
-        elif(P>100):P=100
+        current = ina219.getCurrent_mA()    
+        P = ina219.getPercent(bus_voltage)
 
         # INA219 measure bus voltage on the load side. So PSU voltage = bus_voltage + shunt_voltage
         print("Voltage:  {:6.3f} V".format(bus_voltage))
