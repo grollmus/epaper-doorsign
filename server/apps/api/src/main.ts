5f8290e65df7d2d5998ea3ac14c9cae5b8afc866
Daniel Weiß <daniel.weiss@grollmus.de>
2022-11-03 16:51:45 +0100

    add swagger

--- server/apps/api/src/main.ts
+++ server/apps/api/src/main.ts
@@ -4,19 +4,31 @@
  */
 
 import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document);
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
+  Logger.log(
+    `👾 Find Swagger on: http://localhost:${port}/${globalPrefix}/swagger`
+  )
 }
 
 bootstrap();
